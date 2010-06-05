b9c81fbb0bb5c40c1c1fb38e40be7927e3e57e0b
cxyzs7 <cxyzs7@b4cdb41b-21ee-3654-4928-f1018fbfa5d5>
2010-06-05 06:07:01 +0000

    FireConverter

---
+++ trunk/rendering/pbrt-1.04/fire/main.cpp
@@ -0,0 +1,32 @@
+#include "stdio.h"
+#include "DensityTemperatureVolume.h"
+
+int main(int argc, char* argv[])
+{
+    if (argc == 1)
+    {
+        printf("Please specify the file name");
+        return -1;
+    }
+    int sizeX = 100, sizeY = 100, sizeZ = 40;
+    DensityTemperatureVolume data(argv[1]);
+    FILE* densityFile = fopen("density_test.pbrt", "w");
+    fprintf(densityFile, "Volume \"volumegrid\" \"integer nx\" %d \"integer ny\" %d \"integer nz\" %d\n\"point p0\" [ 0.010000 0.010000 0.010000 ] \"point p1\" [ 1.990000 1.990000 0.790000 ]\n \"float density\" [\n", sizeX, sizeY, sizeZ);
+    float scaleX = (float)(data.getSizeInX()-1)/sizeX;
+    float scaleY = (float)(data.getSizeInY()-1)/sizeY;
+    float scaleZ = (float)(data.getSizeInZ()-1)/sizeZ;
+    for (int k = 0; k < sizeZ; k++)
+    {
+        for (int j = 0; j < sizeY; j++)
+        {
+            for (int i = 0; i < sizeX; i++)
+            {
+                fprintf(densityFile, "%.3f ", data.getDensityAt(i*scaleX, j*scaleY, k*scaleZ));
+            }
+            fprintf(densityFile, "\n");
+        }
+        fprintf(densityFile, "\n");
+    }
+    fprintf(densityFile, "]\n");
+    fclose(densityFile);
+}
