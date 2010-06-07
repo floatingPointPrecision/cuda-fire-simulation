47d35b7911a423fbbf853963089dc9c82b845526
sklesser <sklesser@b4cdb41b-21ee-3654-4928-f1018fbfa5d5>
2010-06-07 02:50:28 +0000

    new simulation and normalization matrix

--- trunk/rendering/pbrt-1.04/fire/whiteTransformGeneration.py
+++ trunk/rendering/pbrt-1.04/fire/whiteTransformGeneration.py
@@ -2,7 +2,7 @@ import numpy
 from math import *
 from numpy import *
 
-maxTemperature = 2500.0
+maxTemperature = 1800.0
 
 # XYZ to LMS transform matrix from http://en.wikipedia.org/wiki/LMS_color_space
 XYZtoLMS = matrix('0.8951 0.2664 -0.1614; -0.7502 1.7135 0.0367; 0.0389 -0.0685 1.0296')
@@ -47,7 +47,7 @@ def getRadiance(waveLength, temperature):
     #print 'wavelength to the 5th',pow(waveLength,5)
     #print 'other denominator part',(exp(exponentValue) - 1)
     radiance = numerator / denominator
-    print 'radiance:',radiance
+    #print 'radiance:',radiance
     return radiance
 
 # generate xyz coordinates for white point
@@ -88,6 +88,61 @@ finalTransformMatrix = XYZtoLMSinv * whitePointDiagMatrix * XYZtoLMS
 print ''
 print 'final transform matrix:'
 print finalTransformMatrix
+
+def XYZtoRGB(x,y,z):
+    xyz = [x,y,z]
+    normalizeList(xyz,3)
+    x=xyz[0]
+    y=xyz[1]
+    z=xyz[2]
+    r = 2.5623 * x + (-1.1661) * y + (-0.3962) * z
+    g = (-1.0215) * x + 1.9778 * y + 0.0437 * z
+    b = 0.0752 * x + (-0.2562) * y + 1.1810 * z
+    return [r,g,b]
+
+def generateRGBfromTemp(temp):
+    xyz = [0, 0, 0]
+    sampleRange = lambdaValues[-1] - lambdaValues[0]
+    sampleIncrement = sampleRange / numSamples
+    for sample in range(numSamples):
+        wavelength = lambdaValues[sample]
+        radiance = getRadiance(wavelength, temp)
+        xyz[0] += cieX[sample]*radiance
+        xyz[1] += cieY[sample]*radiance
+        xyz[2] += cieZ[sample]*radiance
+    return XYZtoRGB(xyz[0],xyz[1],xyz[2])
+
+def generateRGBfromTempWithWhiteNormalization(temp):
+    xyz = [0, 0, 0]
+    sampleRange = lambdaValues[-1] - lambdaValues[0]
+    sampleIncrement = sampleRange / numSamples
+    for sample in range(numSamples):
+        wavelength = lambdaValues[sample]
+        radiance = getRadiance(wavelength, temp)
+        xyz[0] += cieX[sample]*radiance
+        xyz[1] += cieY[sample]*radiance
+        xyz[2] += cieZ[sample]*radiance
+    xyzAsMatrix = matrix('0.0; 0; 0')
+    xyzAsMatrix[0]=xyz[0]
+    xyzAsMatrix[1]=xyz[1]
+    xyzAsMatrix[2]=xyz[2]
+    normalizeList(xyz,3)
+    finalXYZ = finalTransformMatrix * xyzAsMatrix
+    x = int(finalXYZ[0])
+    y = int(finalXYZ[1])
+    z = int(finalXYZ[2])
+    return XYZtoRGB(x,y,z)
+print ''
+print 'RGB values'
+for t in range(50):
+    temp = (t+1) * 50
+    rgb = generateRGBfromTemp(temp)
+    rgb[0]=min(max(rgb[0],0),1)
+    rgb[1]=min(max(rgb[1],0),1)
+    rgb[2]=min(max(rgb[2],0),1)
+    print 'rgb value',temp,rgb[0]*255,rgb[1]*255,rgb[2]*255
+    
+    
 
 
 
